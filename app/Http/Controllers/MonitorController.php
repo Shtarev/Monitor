409286342037276dc54a0375ec07f9b15dcabc0a
Andrey Shtarev <54120707+Shtarev@users.noreply.github.com>
2020-08-04 10:12:45 +0200

    Update MonitorController.php

--- app/Http/Controllers/MonitorController.php
+++ app/Http/Controllers/MonitorController.php
@@ -7,13 +7,13 @@ use App\Monitor\Monitor;
 
 class MonitorController extends Controller
 {
-    // monitorig for back
+
     public function start(){
 		$monitor = new Monitor;
 		$output = $monitor->Back()->output;
 		return view('admin.monitor', $output);
 	}
-    // monitorig for front
+
     public function client(Request $request) {
         $monitor = new Monitor;
 		$monitor->Front();
